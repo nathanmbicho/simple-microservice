6ff486f3d39697537380ceee6ca5bf103f96628d
nathanmbicho <nathanmbicho@gmail.com>
2022-03-17 22:00:41 +0300

    simple welcome function

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello Nathan! Greetings."))
+	if err != nil {
+		return
+	}
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", welcomeHandler)
+
+	log.Println("Starting server on http://localhost:5040")
+	err := http.ListenAndServe(":5040", mux)
+	log.Fatal(err)
+
+}
